686c9714cbb495c02f79140719d1ad4e5167c03c
Tumuluri Shashank <tumulurishashank69@gmail.com>
2026-07-15 16:14:42 +0530

    Update

--- combinational/multiplexers/mux_4x1/mux_4x1.v
+++ combinational/multiplexers/mux_4x1/mux_4x1.v
@@ -4,7 +4,7 @@ module mux_4x1(
     output reg y
 );
 
-always@(*)begin
+always@(*)begin     //Behavioral
     case (sel)
        2'b00 : y=i[0];
        2'b01 : y=i[1];
@@ -17,3 +17,31 @@ always@(*)begin
 end
 
 endmodule
+
+/*
+
+module mux_4x1(
+    input [3:0]i,
+    input [1:0]sel,
+    output y
+);
+
+assign y = ( (~s1 & ~s0 & i[0]) | (~s1 & s0 & i[1]) | (s1 & ~s0 & i[2]) | (s1 & s0 & i[3]) ); //Dataflow
+endmodule
+
+
+//conditional operator
+module mux_4x1(
+    input [3:0]i,
+    input [1:0]sel,
+    output reg  y
+);
+
+always@(*)begin
+    y = (sel[1] & sel[0]) ? i[3] : (sel[1] & ~sel[0])? i[2] : (~sel[1] & sel[0]) ?  i[1] : (~sel[1] & ~sel[0]) ? i[0] : 1'bx; 
+    
+end
+endmodule
+
+*/
+
